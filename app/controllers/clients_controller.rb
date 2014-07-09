3e98839829a1c124129026a8b60ea6d576524f03
chawanangwa lupafya <chawanangwa.lupafya@baobabhealth.org>
2014-07-09 13:29:37 +0200

    initial htc module template

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,64 +1,40 @@
 class ClientsController < ApplicationController
   before_action :set_client, only: [:show, :edit, :update, :destroy]
 
-  # GET /clients
-  # GET /clients.json
   def index
     @clients = Client.all
   end
 
-  # GET /clients/1
-  # GET /clients/1.json
   def show
   end
 
-  # GET /clients/new
   def new
     @client = Client.new
   end
 
-  # GET /clients/1/edit
   def edit
   end
 
-  # POST /clients
-  # POST /clients.json
   def create
     @client = Client.new(client_params)
 
-    respond_to do |format|
-      if @client.save
-        format.html { redirect_to @client, notice: 'Client was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @client }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @client.errors, status: :unprocessable_entity }
-      end
+    if @client.save
+
+    else
+
     end
+
   end
 
-  # PATCH/PUT /clients/1
-  # PATCH/PUT /clients/1.json
   def update
-    respond_to do |format|
-      if @client.update(client_params)
-        format.html { redirect_to @client, notice: 'Client was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @client.errors, status: :unprocessable_entity }
-      end
-    end
+		if @client.update(client_params)
+
+		else
+		end
   end
 
-  # DELETE /clients/1
-  # DELETE /clients/1.json
   def destroy
     @client.destroy
-    respond_to do |format|
-      format.html { redirect_to clients_url }
-      format.json { head :no_content }
-    end
   end
 
   private
