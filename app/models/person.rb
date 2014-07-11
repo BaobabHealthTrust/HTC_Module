6cd4054858ca809ec09923aa6ea538f7f3ad9332
Fchirwa <fuvu.chirwa@gmail.com>
2014-07-11 10:51:33 +0200

    Working on counselor protocols

--- app/models/person.rb
+++ app/models/person.rb
@@ -3,4 +3,10 @@ class Person < ActiveRecord::Base
 		include Openmrs
 
 		before_save :before_create
+
+	def age(current_date = Date.today)
+			age = current_date.year - birthdate.year
+			age -= 1 if birthdate.strftime("%m%d").to_i > current_date.strftime("%m%d").to_i
+			age
+	end
 end
