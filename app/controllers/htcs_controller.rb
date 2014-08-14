2ddc1552157deb8d43ed8eedd857a7fc4439dd8c
chawanangwa lupafya <chawanangwa.lupafya@baobabhealth.org>
2014-08-14 21:01:09 +0200

    update application dashboard

--- app/controllers/htcs_controller.rb
+++ app/controllers/htcs_controller.rb
@@ -65,9 +65,18 @@ class HtcsController < ApplicationController
 			status = "In"
 			if Location.login_rooms_details[r].nil?
 				status = "Out"
+				@dash_board[r][:vg_waiting] = "--"
+				@dash_board[r][:seen_today] = "--"
 			else
 				user_id = Location.login_rooms_details[r][:user_id]
 				@dash_board[r][:username] = User.find(user_id).username
+
+				#Average patient waiting time 
+				@dash_board[r][:vg_waiting] = average_waiting_time_for_user(user_id)
+				
+				#Total patients seen today so far
+				date = session[:datetime].to_date rescue date = Date.today
+				@dash_board[r][:seen_today] = total_seen_today_by_user(user_id, date)
 			end
 			@dash_board[r][:status] = status
 			
@@ -87,9 +96,77 @@ class HtcsController < ApplicationController
 						@dash_board[r][:latest_encounter_date] = datetime
 					end
 			end
-
+			
   	end
   	render layout: false
   end
+  
+  def average_waiting_time_for_user(user_id)
+  		in_waiting = EncounterType.find_by_name('IN WAITING').id
+  		in_session = EncounterType.find_by_name('IN SESSION').id
+  		
+			clients_seen_today = Encounter.find_by_sql(
+					"SELECT in_waiting.patient_id,
+								 in_waiting.encounter_datetime as in_waiting,
+								 in_session.encounter_datetime as in_session
+					 FROM
+							(SELECT * FROM htc_development.encounter
+												WHERE encounter_type = #{in_waiting} AND voided = 0 
+							) AS in_waiting
+							LEFT JOIN
+							(SELECT * FROM htc_development.encounter
+												WHERE encounter_type = #{in_session}
+													AND voided = 0 AND creator = #{user_id}
+							) AS in_session
+							ON in_waiting.patient_id=in_session.patient_id
+									AND DATE(in_waiting.encounter_datetime)=DATE(in_session.encounter_datetime)
+					WHERE in_waiting.patient_id IS NOT NULL AND in_session.patient_id IS NOT NULL"
+				)
+			
+				diff = clients_seen_today.map do |e| 
+								e.in_session.to_i - e.in_waiting.to_i
+				end
+				avg_difference = diff.sum/diff.count
+  			distance_between(avg_difference)
+  end
+  
+  def total_seen_today_by_user(user_id, date=Date.today) 
+  		in_waiting = EncounterType.find_by_name('IN WAITING').id
+  		in_session = EncounterType.find_by_name('IN SESSION').id
+  		
+			clients_seen_today = Encounter.find_by_sql(
+					"SELECT in_waiting.patient_id,
+								 in_waiting.encounter_datetime as in_waiting,
+								 in_session.encounter_datetime as in_session
+					 FROM
+							(SELECT * FROM htc_development.encounter
+												WHERE encounter_type = #{in_waiting} AND voided = 0 
+							) AS in_waiting
+							LEFT JOIN
+							(SELECT * FROM htc_development.encounter
+												WHERE encounter_type = #{in_session}
+													AND voided = 0 AND creator = #{user_id}
+							) AS in_session
+							ON in_waiting.patient_id=in_session.patient_id
+									AND DATE(in_waiting.encounter_datetime)=DATE(in_session.encounter_datetime)
+					WHERE in_waiting.patient_id IS NOT NULL AND in_session.patient_id IS NOT NULL
+								AND DATE(in_session.encounter_datetime) = DATE('#{date}')"
+				).count  
+  end
+  
+  def distance_between(difference)
+    #difference = end_date.to_i - start_date.to_i
+    seconds    =  difference % 60
+    difference = (difference - seconds) / 60
+    minutes    =  difference % 60
+    difference = (difference - minutes) / 60
+    hours      =  difference % 24
+    difference = (difference - hours)   / 24
+    days       =  difference % 7
+    weeks      = (difference - days)    /  7
+    
+    return "#{hours}hr, #{minutes}min, #{seconds}sec"
+  end
+  
 
 end
