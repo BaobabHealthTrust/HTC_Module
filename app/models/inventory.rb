f6de657a675f97c203316e66d230c135f798a162
phundi <kennethkapundi1@gmail.com>
2016-03-09 11:49:19 +0200

    fixed a bug ; damages were not accounted for in stock_reports

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -50,7 +50,7 @@ class Inventory < ActiveRecord::Base
     kit_typesB = []
     if kit.blank?
       kit_typesA = Kit.all.map(&:id)
-      kit_typesB = ["Negative Serum", "Positive Serum", "Negative DTS", "Positive DTS"]
+      kit_typesB = ["Negative Serum", "Positive"]
     else
       kit.each do |k|
         kk = Kit.where(name: k).first rescue nil
@@ -77,7 +77,7 @@ class Inventory < ActiveRecord::Base
     kit_typesB = []
     if kit.blank?
       kit_typesA = Kit.all.map(&:id)
-      kit_typesB = ["Negative Serum", "Positive Serum", "Negative DTS", "Positive DTS"]
+      kit_typesB = ["Negative", "Positive"]
     else
       kit.each do |k|
         kk = Kit.where(name: k).first rescue nil
@@ -97,7 +97,7 @@ class Inventory < ActiveRecord::Base
   def self.remaining_sum(date, lot_number, type)
     del_type = InventoryType.where(name: "Delivery").first.id
     dist_type = InventoryType.where(name: "Distribution").first.id
-    damages_type = InventoryType.where(name: "Damages")
+    damages_type = InventoryType.where(name: "Losses").first.id
 
     if Kit.where(name: type).blank?
        del_type = InventoryType.where(name: "Serum Delivery")
@@ -111,8 +111,8 @@ class Inventory < ActiveRecord::Base
 
     damages =  Inventory.find_by_sql(["SELECT SUM(value_numeric) AS total FROM inventory
                                           WHERE inventory_type = ? AND voided = 0 AND lot_no = ?
-                                          AND DATE(encounter_date) <= ? AND DATE(date_of_expiry) > ?",
-                                      damages_type, lot_number, date, date
+                                          AND DATE(encounter_date) <= ?",
+                                      damages_type, lot_number, date,
                                      ]).first.total.to_i
 
     distribution =  Inventory.find_by_sql(["SELECT SUM(ci.value_numeric) AS total FROM councillor_inventory ci
